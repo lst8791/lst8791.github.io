17f1c8a1444aca27647dc5f4bc7aa4dc4ffaf751
lst8791-_- <83847757+lst8791@users.noreply.github.com>
2021-06-05 12:05:38 +0900

    아주 굿
    6월 5일

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var diary_num =3;
+var diary_idx = 1;
+
 window.onload = function() {
 
      var desc = ["설명이 들어갈 공간입니다.",
@@ -55,4 +58,52 @@ window.onload = function() {
             }, 4000);
         });
         }
+
+    replaceDiary('/diary/diary1.html');
+
+    var diary_left_arrow = document.getElementById("diary_left_arrow");
+    var diary_right_arrow = document.getElementById("diary_right_arrow")
+
+    diary_left_arrow.onclick = diaryLeftClick;
+    diary_right_arrow.onclick = diaryRightClick;
+}
+
+function replaceDiary(filename) {
+    $.get(filename, function(result){
+        var target = document.getElementById("diary_text");
+        var obj = $(result).find('p');
+        target.innerText = obj[0].innerText;
+    });
+
+    $.get(filename, function(result){
+        var target = document.getElementById("diary_title");
+        var obj = $(result).find('h2');
+        target.innerText = obj[0].innerText;
+    });
+
+    $.get(filename, function(result){
+        var target = document.getElementById("diary_img");
+        var obj = $(result).find('img');
+        target.src = obj[0].src;
+    });
+}
+
+function diaryLeftClick(){
+    $('.diary').fadeOut(500, function(){
+         diary_idx--;
+         if (diary_idx < 1) diary_idx = diary_idx = diary_num
+         var html = "/diary/diary" + diary_idx + ".html";
+         replaceDiary(html);
+         $(this).fadeIn(500);
+    });
+}
+
+function diaryRightClick(){
+    $('.diary').fadeOut(500, function(){
+         diary_idx++;
+         if (diary_idx > diary_num) diary_idx = 1;
+         var html = "/diary/diary" + diary_idx + ".html";
+         replaceDiary(html);
+         $(this).fadeIn(500);
+    });
 }
